767ec4f1088e9d8fb62f4508986bf119846e7a30
gigi-se <lpze.gisselleandrea.severich.ar@unifranz.edu.bo>
2025-09-01 22:03:06 -0400

    tarea 5

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Entrada from "./components/entradas";
+import Resultado from "./components/result";
+import "./App.css";
+import reactLogo from "./logo.svg"; 
 
-function App() {
+const App: React.FC = () => {
+  const [parrafo, setParrafo] = useState("");
+  const [palabra, setPalabra] = useState("");
+
+  const handleSearch = (nuevoParrafo: string, nuevaPalabra: string) => {
+    setParrafo(nuevoParrafo);
+    setPalabra(nuevaPalabra);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <div>
+      <div style={{ textAlign: "center", marginTop: "50px" }}>
+        <img src={reactLogo}  style={{ width: "100px" }} />
+      </div>
+      <Entrada onSearch={handleSearch} />
+      <Resultado parrafo={parrafo} palabra={palabra} />
     </div>
   );
-}
+};
 
 export default App;
